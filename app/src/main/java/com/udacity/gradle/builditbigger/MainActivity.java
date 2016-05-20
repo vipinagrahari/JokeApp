c3085c622e6bd1c759726c61111aa6f753252a79
vipin agrahari <vipinkagrahari@gmail.com>
2016-05-20 18:32:19 +0530

    Create GCE Module and set up a Google Cloud Endpoints development server to pull our jokes from there.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +13,14 @@ import android.widget.Toast;
 
 import com.example.Joker;
 import com.example.www.jokeview.JokeActivity;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
+
+import java.io.IOException;
+
+import io.github.vipinagrahari.jokeapp.backend.myApi.MyApi;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -43,11 +54,17 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view){
+    public void tellJoke1(View view){
         Intent intent=new Intent(MainActivity.this, JokeActivity.class);
         intent.putExtra("joke",new Joker().getJoke());
         startActivity(intent);
     }
+    public void tellJoke(View v){
+
+        new EndpointsAsyncTask().execute(MainActivity.this);
+
+    }
+
 
 
 }
--- javajokes/src/main/java/com/example/Joker.java
+++ javajokes/src/main/java/com/example/Joker.java
@@ -7,8 +7,6 @@ public class Joker {
     }
 
 
-
-
 }
 
 
